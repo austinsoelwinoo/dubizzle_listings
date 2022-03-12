01063c85b0553d3744db5fb965fb2ccbe1f8de65
Austin.SoeLwin <soelwin@umich.edu>
2022-03-12 22:20:08 +0700

    -set up Jetpack compose simple list ui

--- app/src/main/java/com/dubizzle/listings/presentation/extensions.kt
+++ app/src/main/java/com/dubizzle/listings/presentation/extensions.kt
@@ -1,7 +1,28 @@
 package com.dubizzle.listings.presentation
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.util.DisplayMetrics
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.Glide
+import com.dubizzle.listings.R
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 fun calculateNoOfColumns(
     context: Context,
@@ -12,3 +33,91 @@ fun calculateNoOfColumns(
     return (screenWidthDp / columnWidthDp + 0.5).toInt()
 }
 
+@Composable
+fun NetworkImageComponentGlide(
+    url: String, modifier: Modifier = Modifier
+) {
+    // Reacting to state changes is the core behavior of Compose. You will notice a couple new
+    // keywords that are compose related - remember & mutableStateOf.remember{} is a helper
+    // composable that calculates the value passed to it only during the first composition. It then
+    // returns the same value for every subsequent composition. Next, you can think of
+    // mutableStateOf as an observable value where updates to this variable will redraw all
+    // the composable functions that access it. We don't need to explicitly subscribe at all. Any
+    // composable that reads its value will be recomposed any time the value
+    // changes. This ensures that only the composables that depend on this will be redraw while the
+    // rest remain unchanged. This ensures efficiency and is a performance optimization. It
+    // is inspired from existing frameworks like React.
+    var image by remember { mutableStateOf<ImageBitmap?>(null) }
+    var drawable by remember { mutableStateOf<Drawable?>(null) }
+    val sizeModifier = modifier
+        .fillMaxWidth()
+        .sizeIn(maxHeight = 200.dp)
+
+    // LocalContext is a LocalComposition for accessting the context value that we are used to using
+    // in Android.
+
+    // LocalComposition is an implicit way to pass values down the compose tree. Typically, we pass values
+    // down the compose tree by passing them as parameters. This makes it easy to have fairly
+    // modular and reusable components that are easy to test as well. However, for certain types
+    // of data where multiple components need to use it, it makes sense to have an implicit way
+    // to access this data. For such scenarios, we use LocalComposition. In this example, we use the
+    // LocalContext to get hold of the Context object. In order to get access to the latest
+    // value of the LocalComposition, use the "current" property eg - LocalContext.current. Some other
+    // examples of common LocalComposition's are LocalTextInputService,LocalDensity, etc.
+    val context = LocalContext.current
+    // Sometimes we need to make changes to the state of the app. For those cases, Composes provides
+    // some Effect API's which provide a way to perform side effects in a predictable manner.
+    // DisposableEffect is one such side effect API that provides a mechanism to perform some
+    // clean up actions if the key to the effect changes or if the composable leaves composition.
+    DisposableEffect(url) {
+        val glide = Glide.with(context)
+        val target = object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) {
+                image = null
+                drawable = placeholder
+            }
+
+            override fun onResourceReady(bitmap: Bitmap, transition: Transition<in Bitmap>?) {
+                image = bitmap.asImageBitmap()
+            }
+        }
+        glide
+            .asBitmap()
+            .load(url)
+            .placeholder(R.drawable.listing_item_placeholder)
+            .into(target)
+
+        onDispose {
+            image = null
+            drawable = null
+            glide.clear(target)
+        }
+    }
+
+    val theImage = image
+    val theDrawable = drawable
+    if (theImage != null) {
+        // Column is a composable that places its children in a vertical sequence. You
+        // can think of it similar to a LinearLayout with the vertical orientation.
+        // In addition we also pass a few modifiers to it.
+
+        // You can think of Modifiers as implementations of the decorators pattern that are
+        // used to modify the composable that its applied to.
+        Column(
+            modifier = sizeModifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Image is a pre-defined composable that lays out and draws a given [ImageBitmap].
+            Image(bitmap = theImage, contentDescription = null)
+        }
+    } else if (theDrawable != null) {
+        // We use the Canvas composable that gives you access to a canvas that you can draw
+        // into. We also pass it a modifier.
+        Canvas(modifier = sizeModifier) {
+            drawIntoCanvas { canvas ->
+                theDrawable.draw(canvas.nativeCanvas)
+            }
+        }
+    }
+}
--- app/src/main/java/com/dubizzle/listings/presentation/list/DListActivity.kt
+++ app/src/main/java/com/dubizzle/listings/presentation/list/DListActivity.kt
@@ -1,65 +1,93 @@
 package com.dubizzle.listings.presentation.list
 
 import android.os.Bundle
-import android.view.View
+import com.dubizzle.listings.R
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import coil.compose.AsyncImage
 import com.dubizzle.core.domain.Listing
-import com.dubizzle.listings.databinding.ActivityListBinding
-import com.dubizzle.listings.presentation.calculateNoOfColumns
-import com.dubizzle.listings.presentation.detail.DDetailsActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DListActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityListBinding
     private val modelD: DListViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityListBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-
-        val mNoOfColumns: Int = calculateNoOfColumns(applicationContext, 130.0f)
-        val mGridLayoutManager = GridLayoutManager(applicationContext, mNoOfColumns)
-        val dListAdapter =
-            DListAdapter(applicationContext, object : DListAdapter.ActionClickListener {
-                override fun clicked(listing: Listing) {
-                    val currentActivity = this@DListActivity
-                    if (currentActivity.lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) {
-                        currentActivity.startActivity(
-                            DDetailsActivity.getCallingIntent(
-                                currentActivity,
-                                listing
-                            )
-                        )
-                    }
-                }
-            })
-
-        binding.recycler.adapter = dListAdapter
-        binding.recycler.layoutManager = mGridLayoutManager
-
-        modelD.listings.observe(this) {
-            handleViewState(false, it)
-            dListAdapter.items = it
+        setContent {
+            LiveDataComponent(modelD.listings)
         }
+        modelD.loadDocuments()
+    }
 
-        if (modelD.listings.value?.isEmpty() != false) {
-            handleViewState(true)
-            modelD.loadDocuments()
+    @Composable
+    fun LiveDataComponent(listingsLiveData: LiveData<List<Listing>>) {
+        val listings by listingsLiveData.observeAsState(initial = emptyList())
+        if (listings.isNotEmpty()) {
+            ListingList(listings)
         }
     }
 
-    private fun handleViewState(isLoading: Boolean, items: List<Listing> = emptyList()) {
-        if (isLoading) {
-            binding.pbLoading.visibility = View.VISIBLE
-            binding.recycler.visibility = View.GONE
-            binding.tvEmpty.visibility = View.GONE
-        } else {
-            binding.pbLoading.visibility = View.GONE
-            binding.recycler.visibility = if (items.isNotEmpty()) View.VISIBLE else View.GONE
-            binding.tvEmpty.visibility = if (items.isEmpty()) View.VISIBLE else View.GONE
+    @Composable
+    fun ListingList(listings: List<Listing>) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(4.dp)
+        ) {
+            items(listings) { listing ->
+                PlantCard(listing.name, listing.price, listing.retrieveFirstImageThumbnail())
+            }
+        }
+    }
+
+    @Composable
+    fun PlantCard(name: String, description: String, imageUrl: String) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            shape = MaterialTheme.shapes.medium,
+            elevation = 5.dp,
+            backgroundColor = MaterialTheme.colors.surface
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                AsyncImage(
+                    model = imageUrl,
+                    placeholder = painterResource(id = R.drawable.listing_item_placeholder),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(0.dp),
+                    contentScale = ContentScale.Fit,
+                )
+                Column(Modifier.padding(8.dp)) {
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h6,
+                        color = MaterialTheme.colors.onSurface,
+                    )
+                    Text(
+                        text = description,
+                        style = MaterialTheme.typography.body2,
+                    )
+                }
+            }
         }
     }
 }
